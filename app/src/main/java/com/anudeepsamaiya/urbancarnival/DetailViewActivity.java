0a743e6670793770b3957dc1bb49cffb242041ae
Anudeep <anudeepsamaiya@gmail.com>
2017-01-17 22:21:11 +0530

    added permission utils

---
+++ app/src/main/java/com/anudeepsamaiya/urbancarnival/DetailViewActivity.java
@@ -0,0 +1,18 @@
+package com.anudeepsamaiya.urbancarnival;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by anudeepsamaiya on 15/1/17.
+ */
+
+public class DetailViewActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
